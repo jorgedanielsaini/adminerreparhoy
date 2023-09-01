7602bf9d8f179107ea94fe1d53f54c7c9bcdc328
Daniel <jorgedanielsaini@gmail.com>
2023-09-01 14:07:44 -0300

    cambio url api

--- errepar_hoy/settings.py
+++ errepar_hoy/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'mail_diario',
     'ereports',
     'rest_framework',    
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -67,7 +68,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'errepar_hoy.urls'
 
